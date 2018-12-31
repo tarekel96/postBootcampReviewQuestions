0d44c8e3eff39d39688e60d2f3dfa3da22e6a1fe
Tarek ElHajjaoui <tarekel96@gmail.com>
2018-12-30 18:21:47 -0800

    figured out maximin and maximax prob think i have concept of for loop scope down now

--- maximin/Solved/maximin.js
+++ maximin/Solved/maximin.js
@@ -1,16 +1,20 @@
-'use strict';
+"use strict";
 
-function maximin (arr) {
-  var minimum = maximum = arr[0];
+function maximin(arr) {
+  // var minimum = (maximum = arr[0]);
+  let maximum = arr[0];
+  let minimum = arr[0];
 
   for (var i = 0; i < arr.length; i += 1) {
     var candidate = arr[i];
 
-    if (candidate > maximum)
-      maximum = candidate;
-    if (candidate < minimum)
-      minimum = candidate;
+    if (candidate > maximum) maximum = candidate;
+    if (candidate < minimum) minimum = candidate;
   }
+  console.log(`minimum: ${minimum} maximum: ${maximum}`);
 
   return [minimum, maximum];
 }
+
+let givenArray = [10, 2, 3, 8, 12, 91, 310];
+maximin(givenArray);
--- maximin/maximin.js
+++ maximin/maximin.js
@@ -5,25 +5,48 @@ from an array _without_ using the `Math.min` or `Math.max` methods. */
 
 // My Answer
 
-let givenArray = [10, 2, 3, -8, 12, 91, 310];
+let givenArray = [10, 2, 3, 8, 12, 91, 310];
 
-let findMax = function(array) {
+/* let findMax = function(array) {
+  let highestNumber = 0;
+  let lowestNumber = 0;
   for (let i = 0; i < array.length; i++) {
     let positiveArray = [];
     positiveArray = Math.abs(array[i]);
     console.log(`positive array: ${positiveArray}`);
-    let highestNumber = 0;
-    if (positiveArray[i] % 2 > positiveArray[i + 1] % 2) {
-      highestNumber = positiveArray[i] % 2;
-      console.log(`Highest Number: ${highestNumber}`);
-    } else if (positiveArray[i + 1] % 2 > positiveArray[i] % 2) {
-      highestNumber = positiveArray[i + 1] % 2;
-      console.log(`Highest Number: ${highestNumber}`);
+    // let highestNumber = 0;
+    // let lowestNumber = 0;
+    if (positiveArray[i] > highestNumber) {
+      // console.log("hello world");
+      // console.log(highestNumber);
+      highestNumber = positiveArray[i];
+      // console.log(`Highest number: ${highestNumber}`);
+    } else if (positiveArray[i] < lowestNumber) {
+      lowestNumber = positiveArray[i];
     }
-    console.log(`Highest Number: ${highestNumber}`);
   }
-};
+  console.log(`Highest number: ${highestNumber}`);
+  return `Highest number: ${highestNumber} & Lowest Number: ${lowestNumber}`;
+}; */
 
-findMax(givenArray);
+// findMax(givenArray);
 
 // console.log(Math.abs(givenArray[i]));
+
+let findMaxAndMin = function(array) {
+  let maxNumber = array[0];
+  let minNumber = array[0];
+  for (let i = 0; i < array.length; i++) {
+    let candidate = array[i];
+    if (candidate > maxNumber) {
+      maxNumber = candidate;
+    }
+    if (candidate < minNumber) {
+      minNumber = candidate;
+    }
+  }
+  console.log(`Max Number: ${maxNumber} Min Number: ${minNumber}`);
+  return `Max Number: ${maxNumber} Min Number: ${minNumber}`;
+};
+
+findMaxAndMin(givenArray);
