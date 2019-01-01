755583a81acde0ec0c5253e80d39c69dbb47bba1
Tarek ElHajjaoui <tarekel96@gmail.com>
2019-01-01 10:15:22 -0800

    still did not finish recursive 100 percent but added a for loop and seem to be close

--- recursive_max_element/recursiveMaxElement.js
+++ recursive_max_element/recursiveMaxElement.js
@@ -33,20 +33,43 @@ let recursiveMax = function(array) {
 
 recursiveMax(givenArray); */
 
+// let givenArray = [1, 4, 6, 2, 6, 10, 12, 96, 30, 50, 60];
+
+// let recursiveMax = function(array) {
+//   let i = 0;
+//   while (i < array.length) {
+//     let iterator = array[i];
+//     let base = array[0];
+//     if (iterator > base) {
+//       base = iterator;
+//       i++;
+//     } else if (iterator >= base) {
+//       i++;
+//     }
+//     console.log(`Highest number: ${base}`);
+//   }
+// };
+
+// recursiveMax(givenArray);
+
 let givenArray = [1, 4, 6, 2, 6, 10, 12, 96, 30, 50, 60];
 
 let recursiveMax = function(array) {
   let i = 0;
   while (i < array.length) {
     let iterator = array[i];
-    let base = array[0];
-    if (iterator > base) {
-      base = iterator;
-      i++;
-    } else if (iterator >= base) {
-      i++;
+    for (let j = 0; j < array.length; j++) {
+      let base = array[j];
+      console.log(`base: ${base}`);
+      if (iterator > base) {
+        base = iterator;
+        i++;
+      } else if (iterator >= base) {
+        i++;
+      }
+      //       console.log(`Highest number: ${base}`);
+      //       return `Highest number: ${base}`;
     }
-    console.log(`Highest number: ${base}`);
   }
 };
 
