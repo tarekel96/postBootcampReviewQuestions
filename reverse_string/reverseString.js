606384101a544825b8dba3c395f33e3ec8b72281
Tarek ElHajjaoui <tarekel96@gmail.com>
2019-01-01 11:34:02 -0800

    figured out how to reverse whole word into single array

--- reverse_string/reverseString.js
+++ reverse_string/reverseString.js
@@ -9,14 +9,28 @@ reverse('foobar') // 'raboof'
 
 // My Answer
 
+// let givenWord = "tarek";
+
+// let reverse = function(word) {
+//   for (let i = word.length - 1; i >= 0; i--) {
+//     let reverseArray = [];
+//     let reverseLetters = "";
+//     reverseArray = reverseLetters.concat(word[i]);
+//     console.log(reverseArray);
+//   }
+// };
+
+// reverse(givenWord);
+
 let givenWord = "tarek";
 
 let reverse = function(word) {
   for (let i = word.length - 1; i >= 0; i--) {
-    let reverseArray = [];
+    let newArray = [];
     let reverseLetters = "";
-    reverseArray = reverseLetters.concat(word[i]);
-    console.log(reverseArray);
+    //     console.log(reverseLetters);
+    newArray.push(reverseLetters.concat(word[i]));
+    console.log(newArray[0]);
   }
 };
 
